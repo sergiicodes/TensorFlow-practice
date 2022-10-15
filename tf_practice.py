3b6126fb8495e6f2f879e734b23e14aaaf2fdf90
Sergio Hernandez-Acosta <79073281+sergiojhernandezacosta@users.noreply.github.com>
2022-10-15 18:24:08 -0500

    Update tf_practice.py

--- tf_practice.py
+++ tf_practice.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf   #I was getting a ModuleNotFound error for '.placeholder', otherwise I would simply import tensorflow as tf
+tf.disable_v2_behavior()    
 import numpy as np
 
 INPUT_NEURONS = 2
@@ -8,24 +9,24 @@ OUTPUT_NEURONS = 1
 x_data = np.array([[0,0],[0,1],[1,1]])
 y_data = np.array([0,1,1])
 
-x = tf.placeholder(tf.float32)
-y = tf.placeholder(tf.float32)
+x = tf.compat.v1.placeholder(tf.float32)
+y = tf.compat.v1.placeholder(tf.float32)
 
 h1_layer = {
     'weight': tf.Variable(np.random.rand(INPUT_NEURONS, HIDDEN_LAYER_1_NEURONS).astype(np.float32)),
     'bias' : tf.constant(0.0)
 }
 output_layer = {
-    'weight': tf.Variable(np.random.rand(INPUT_NEURONS, HIDDEN_LAYER_1_NEURONS).astype(np.float32)),
+    'weight': tf.Variable(np.random.rand(HIDDEN_LAYER_1_NEURONS, OUTPUT_NEURONS).astype(np.float32)),
     'bias' : tf.constant(0.0)
 }
 
-h1 = tf.matmul(x,h1_layer['weight']) + h1_layer['bias']
+h1 = tf.matmul(x, h1_layer['weight']) + h1_layer['bias']
 h1 = tf.sigmoid(h1)
 
 predict = tf.matmul(h1, output_layer['weight']) + output_layer['bias']
 
-loss = tf.matmul(tf.square(predict-y))
+loss = tf.reduce_mean(tf.square(predict-y))
 optimizer = tf.train.GradientDescentOptimizer(0.00001).minimize(loss)
 
 sess = tf.Session()
